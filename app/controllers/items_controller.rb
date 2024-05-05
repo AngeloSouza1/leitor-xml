329e96c4dd5a86113a9a25de033cb5c26f78a407
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-05-05 11:14:46 -0300

    chore: ajuste da div auxiliar

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,30 +1,36 @@
-
 class ItemsController < ApplicationController
   def index
-    diretorio = '/home/angelosouza/Projetos /Leitor-Xml/xmls'
+    diretorio = '/home/angelosouza/Projetos/Leitor-Xml/xmls'
     @xml_files = Dir.glob("#{diretorio}/*.xml").map { |file| File.basename(file) }
   end
 
   def reiniciar
-
     redirect_to root_path
   end
-  def processar
-    # Adiciona uma mensagem de log
-    puts "Chamando a função processar"
 
-    # Chama o script Ruby que processa o XML e retorna uma mensagem
-    result = `ruby lib/processar_xml.rb`
-    puts result
-    # Define a mensagem da flash com base na saída do script
-    if result.include?("Não há arquivos XML no diretório.")
-      flash[:alert] = result
+  def processar
+    diretorio = '/home/angelosouza/Projetos/Leitor-Xml/xmls'
+    xml_files = Dir.glob("#{diretorio}/*.xml")
+
+    if xml_files.empty?
+      flash[:alert] = "Não há arquivos XML no diretório."
     else
-       flash[:notice] = result
+      # Adiciona uma mensagem de log
+      puts "Chamando a função processar"
+
+      # Chama o script Ruby que processa o XML e retorna uma mensagem
+      result = `ruby lib/processar_xml.rb`
+      puts result
+
+      # Define a mensagem da flash com base na saída do script
+      if result.include?("Não há arquivos XML no diretório.")
+        flash[:alert] = result
+      else
+        flash[:notice] = result
+      end
     end
 
     # Redireciona para a página inicial
     redirect_to root_path
   end
 end
-
